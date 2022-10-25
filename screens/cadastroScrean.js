abac5f46aac7d262ee12414e0b8a76de1d15c3f7
MatheusCruzMoura <matheuscruz.moura27@gmail.com>
2022-10-25 00:01:40 -0300

    tela de cadastro

--- screens/cadastroScrean.js
+++ screens/cadastroScrean.js
@@ -5,42 +5,50 @@ import { Avatar, Input, Button } from "react-native-elements";
 // import Icon from 'react-native-vector-icons/FontAwesome';
 
 function CadastroScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Input
-        label='Seu endereço de email'
-        placeholder='exemplo@email.com'
-        leftIcon={{ type: 'font-awesome', name: 'envelope', color: '#999' }}
-        containerStyle={{ marginBottom: 0, width: 300 }}
-      />
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start', marginTop: 50 }}>
+            <Input
+                label='Nome'
+                placeholder='Nome completo'
+                leftIcon={{ type: 'font-awesome', name: 'user', color: '#999' }}
+                containerStyle={{ marginBottom: 0, width: 300 }}
+            />
 
-      <Input
-        label='Senha'
-        placeholder='Senha'
-        leftIcon={{
-          type: 'font-awesome',
-          name: 'lock',
-          color: '#999'
-        }}
-        secureTextEntry={true}
-        containerStyle={{ width: 300 }}
-      />
+            <Input
+                label='CPF'
+                placeholder='***.***.***-**'
+                leftIcon={{ type: 'font-awesome', name: 'id-card', color: '#999' }}
+                containerStyle={{ marginBottom: 0, width: 300 }}
+            />
 
-      <Button
-        title="Login"
-        onPress={() => navigation.navigate('Contatos')}
-        buttonStyle={{ width: 290, marginBottom: 10 }}
-      />
+            <Input
+                label='Seu endereço de email'
+                placeholder='exemplo@email.com'
+                leftIcon={{ type: 'font-awesome', name: 'envelope', color: '#999' }}
+                containerStyle={{ marginBottom: 0, width: 300 }}
+            />
 
-      <Button
-        title="Cadastrar-se"
-        type="outline"
-        buttonStyle={{ width: 290 }}
-      />
+            <Input
+                label='Senha'
+                placeholder='Senha'
+                leftIcon={{
+                    type: 'font-awesome',
+                    name: 'lock',
+                    color: '#999'
+                }}
+                secureTextEntry={true}
+                containerStyle={{ width: 300 }}
+            />
+
+            <Button
+                title="Salvar"
+                // onPress={() => navigation.navigate('Contatos')}
+                buttonStyle={{ width: 290, marginTop: 10 }}
+            />
 
 
-    </View>
-  );
+        </View>
+    );
 }
 
 export default CadastroScreen;
